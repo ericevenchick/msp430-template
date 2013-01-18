fc68d31dee193f5567835a12148134ac34f9392f
Eric Evenchick <eric@evenchick.com>
2013-01-18 17:50:25 -0500

    changed default target to msp430g2231, make demo blink LED

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
 /*
  * Example main.c file for MSP430
+ * Blinks red LED on the MSP430 launchpad
  */
 
 #include <msp430.h>
 
 int main() {
-    P1DIR = 0x1;
-    P1OUT = 0x1;
-    return 0;
+    // halt watchdog
+    WDTCTL = WDTPW + WDTHOLD;
+    // make all port 1 pins outputs
+    P1DIR = 0xFF;
+    for (;;) {
+        P1OUT ^= 0x1;
+        __delay_cycles(100000);
+    }
 }
